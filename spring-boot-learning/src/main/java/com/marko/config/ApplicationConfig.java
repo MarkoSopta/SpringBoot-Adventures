2dbcb67f0f3ed63510ecd37bc8514474dce626b7
MarkoSopta <philoyd99@gmail.com>
2023-02-13 19:44:24 +0100

    SecurityConfig,UserDetailsService and AuthenticationProvider

--- spring-boot-learning/src/main/java/com/marko/config/ApplicationConfig.java
+++ spring-boot-learning/src/main/java/com/marko/config/ApplicationConfig.java
@@ -1,12 +1,15 @@
 package com.marko.config;
 
-
 import com.marko.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -18,5 +21,18 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService(){
         return username -> repository.findByEmail(username)
                 .orElseThrow(() ->new UsernameNotFoundException("User not Found"));
+    }
+
+    @Bean
+    public AuthenticationProvider  authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+
+    }
+@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
