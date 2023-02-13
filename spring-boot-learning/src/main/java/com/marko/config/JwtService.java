e00e9f860104e72975afc8dedc7fc6182aac179f
MarkoSopta <philoyd99@gmail.com>
2023-02-13 19:09:11 +0100

    few methods to validate and check the token

--- spring-boot-learning/src/main/java/com/marko/config/JwtService.java
+++ spring-boot-learning/src/main/java/com/marko/config/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -46,6 +47,21 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    //method to validate token
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()))&& !isTokenExpired(token);
+    }
+    //checking if the token expired
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    //getting the expiration date from the token
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
     }
 
 
