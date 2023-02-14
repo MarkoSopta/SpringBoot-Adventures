944fa2060b5957122b15a32f3607f30b342a86a9
MarkoSopta <philoyd99@gmail.com>
2023-02-14 17:41:26 +0100

    authManager

--- spring-boot-learning/src/main/java/com/marko/config/SecurityConfig.java
+++ spring-boot-learning/src/main/java/com/marko/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
     }
