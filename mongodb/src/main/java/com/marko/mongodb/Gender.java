410dcf365a91d474ffc5408837484490d24185c5
MarkoSopta <philoyd99@gmail.com>
2023-02-17 18:06:31 +0100

    creating some data

---
+++ mongodb/src/main/java/com/marko/mongodb/Gender.java
@@ -0,0 +1,5 @@
+package com.marko.mongodb;
+
+public enum Gender {
+    Male,Female
+}
--- mongodb/src/main/java/com/marko/mongodb/Student.java
+++ mongodb/src/main/java/com/marko/mongodb/Student.java
@@ -1,5 +1,23 @@
 package com.marko.mongodb;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Data
+@Document
 public class Student {
-    
+    @Id
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Gender gender;
+    private Address address;
+    private List<String> favSubs;
+    private BigDecimal totalSpentInBooks;
+    private ZonedDateTime created;
 }
