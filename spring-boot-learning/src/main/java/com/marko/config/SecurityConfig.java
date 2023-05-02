4f11d47409eaf1e39e56f2b16f8c77a2cd301f6b
MarkoSopta <philoyd99@gmail.com>
2023-05-02 19:49:15 +0200

    Secured endpoints

--- spring-boot-learning/src/main/java/com/marko/config/SecurityConfig.java
+++ spring-boot-learning/src/main/java/com/marko/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.marko.config;
 
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.sql.Delete;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,12 +14,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.marko.user.Perms.*;
+import static com.marko.user.Role.ADMIN;
+import static com.marko.user.Role.MANAGER;
+import static org.springframework.http.HttpMethod.*;
 /*
- * This is a Java configuration class for setting up Spring Security. 
- * It imports the Jakarta Servlet Filter, Lombok's RequiredArgsConstructor, and several Spring Security classes. 
- * The @Configuration and @EnableWebSecurity annotations indicate that this is a configuration class for web security. 
- * The class has two fields: jwtAuthFilter and authenticationProvider. The securityFilterChain() method configures the HttpSecurity object to disable CSRF, 
- * permit all requests to /api/v1/auth/, require authentication for all other requests, set session creation policy to stateless, 
+ * This is a Java configuration class for setting up Spring Security.
+ * It imports the Jakarta Servlet Filter, Lombok's RequiredArgsConstructor, and several Spring Security classes.
+ * The @Configuration and @EnableWebSecurity annotations indicate that this is a configuration class for web security.
+ * The class has two fields: jwtAuthFilter and authenticationProvider. The securityFilterChain() method configures the HttpSecurity object to disable CSRF,
+ * permit all requests to /api/v1/auth/, require authentication for all other requests, set session creation policy to stateless,
  * and add the jwtAuthFilter before the UsernamePasswordAuthenticationFilter.
  */
 
@@ -32,26 +39,41 @@ public class SecurityConfig {
     private final LogoutHandler logoutHandler;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-    http
-            .csrf()
-            .disable()
-            .authorizeHttpRequests()
-            .requestMatchers("/api/v1/auth/**")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .logout()
-            .logoutUrl("/api/v1/auth/logout")
-            .addLogoutHandler(logoutHandler)
-            .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**")
+                .permitAll()
 
-    return http.build();
+                .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
+
+                .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+                .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+                .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+                .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name()).requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+
+                .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+
+                .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+                .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+                .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+                .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
+
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/v1/auth/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+
+        return http.build();
     }
 }
